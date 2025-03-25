42be03280dc4dd50bc35de58d9477e2a1f9a584a
Evgeny Pisarenko <episarenko@ugcs.com>
2025-03-25 16:03:31 +0100

    Add drag and drop hint label (#223)

--- src/main/java/com/ugcs/gprvisualizer/app/SceneContent.java
+++ src/main/java/com/ugcs/gprvisualizer/app/SceneContent.java
@@ -1,7 +1,16 @@
 package com.ugcs.gprvisualizer.app;
 
+import com.ugcs.gprvisualizer.app.events.FileClosedEvent;
+import com.ugcs.gprvisualizer.app.ext.FileManager;
+import com.ugcs.gprvisualizer.event.FileOpenedEvent;
+import com.ugcs.gprvisualizer.gpr.Model;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.TextAlignment;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javafx.scene.control.SplitPane;
@@ -24,6 +33,11 @@ public class SceneContent extends BorderPane implements InitializingBean {
 	
 	@Autowired
 	private OptionPane optionPane;
+
+	@Autowired
+	private Model model;
+
+	private Label openHint = createOpenHint();
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
@@ -31,7 +45,6 @@ public class SceneContent extends BorderPane implements InitializingBean {
 		this.setOnDragDropped(loader.getDropHandler());
 		this.setCenter(createSplitPane());
 		this.setBottom(statusBar);
-		
 	}
 
 	private SplitPane createSplitPane() {
@@ -42,12 +55,42 @@ public class SceneContent extends BorderPane implements InitializingBean {
 		sp.getItems().add(mapView.getCenter());
 		
 		//profile view
-		sp.getItems().add(profileView.getCenter());
+		StackPane profilePane = new StackPane(profileView.getCenter(), openHint);
+		sp.getItems().add(profilePane);
 		
 		//options tabs
 		sp.getItems().add(optionPane);
 		
 		return sp;
 	}
-	
+
+	private Label createOpenHint() {
+		Label openHint = new Label("""
+			Drag and drop data files here or use \
+			the "Open files" button on the toolbar
+			""");
+		openHint.setTextAlignment(TextAlignment.CENTER);
+		openHint.setWrapText(true);
+		openHint.setMaxWidth(300);
+		openHint.setMouseTransparent(true);
+		return openHint;
+	}
+
+	private void updateOpenHintVisibility() {
+		FileManager fileManager = model.getFileManager();
+		int numOpenFiles = fileManager.getFilesCount();
+		Platform.runLater(() -> {
+			openHint.setVisible(numOpenFiles == 0);
+		});
+	}
+
+	@EventListener
+	private void fileClosed(FileClosedEvent event) {
+		updateOpenHintVisibility();
+	}
+
+	@EventListener
+	private void fileOpened(FileOpenedEvent event) {
+		updateOpenHintVisibility();
+	}
 }

4e9a2b33336f98fdce6e42970a9b1816c1de374f
Evgeny Pisarenko <episarenko@ugcs.com>
2025-05-29 19:03:26 +0200

    Wrap error dialog message text (#300)

--- src/main/java/com/ugcs/gprvisualizer/app/MessageBoxHelper.java
+++ src/main/java/com/ugcs/gprvisualizer/app/MessageBoxHelper.java
@@ -2,10 +2,12 @@ package com.ugcs.gprvisualizer.app;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 
 public class MessageBoxHelper {
+
+	private static final double CONTENT_WIDTH = 400.0;
 
 	public static void showError(String header, String msg) {
 		
@@ -15,11 +17,14 @@ public class MessageBoxHelper {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Error");
 			alert.setHeaderText(header);
-			alert.setContentText(msg);
-			alert.showAndWait();//.ifPresent(rs -> {
 
-			//});
+			Label content = new Label(msg);
+			content.setWrapText(true);
+			content.setPrefWidth(CONTENT_WIDTH);
+			content.setMaxWidth(CONTENT_WIDTH);
+
+			alert.getDialogPane().setContent(content);
+			alert.showAndWait();
 		});
-	}	
-	
+	}
 }

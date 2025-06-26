5bb9060bbdf0e720a74c0bc011ad3ba22e323ece
Evgeny Pisarenko <episarenko@ugcs.com>
2025-06-26 16:02:03 +0200

    Remove unused classes (#327)

--- src/main/java/com/ugcs/gprvisualizer/app/commands/ProgressCommand.java
+++
@@ -1,9 +0,0 @@
-package com.ugcs.gprvisualizer.app.commands;
-
-import com.github.thecoldwine.sigrun.common.ext.TraceFile;
-import com.ugcs.gprvisualizer.app.ProgressListener;
-
-public interface ProgressCommand extends BaseCommand {
-	
-	void execute(TraceFile file, ProgressListener listener);
-}

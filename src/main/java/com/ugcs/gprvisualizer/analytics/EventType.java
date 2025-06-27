6384a2e51807d6df95dd810f467b70cf8a35ffc1
Vladislav Groshkov <vgroshkov@ugcs.com>
2025-06-27 17:35:07 +0200

    Add file opened and file opened error event tracking (#336)

--- src/main/java/com/ugcs/gprvisualizer/analytics/EventType.java
+++ src/main/java/com/ugcs/gprvisualizer/analytics/EventType.java
@@ -2,7 +2,8 @@ package com.ugcs.gprvisualizer.analytics;
 
 public enum EventType {
     APP_STARTED("geohammer-app-started"),
-    ;
+    FILE_OPENED("geohammer-file-opened"),
+    FILE_OPENED_ERROR("geohammer-file-opened-error");
 
     private final String code;
 

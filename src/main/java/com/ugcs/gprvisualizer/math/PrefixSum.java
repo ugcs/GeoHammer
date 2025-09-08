f4078c9a0a9c9386b3cc00180d79eb09091b512f
Evgeny Pisarenko <episarenko@ugcs.com>
2025-09-08 14:12:33 +0300

    Count values in a statistics view in a selected context: view area/line/file (#416)

--- src/main/java/com/ugcs/gprvisualizer/math/PrefixSum.java
+++ src/main/java/com/ugcs/gprvisualizer/math/PrefixSum.java
@@ -7,20 +7,38 @@ import java.util.List;
 public class PrefixSum {
 
     private final double[] prefixSum;
+    private final int[] prefixCount;
 
     public PrefixSum(List<Double> values) {
         Check.notNull(values);
 
         int n = values.size();
         this.prefixSum = new double[n + 1];
+        this.prefixCount = new int[n + 1];
+
         for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + values.get(i);
+            Double value = values.get(i);
+            boolean isValid = value != null && !Double.isNaN(value);
+
+            prefixSum[i + 1] = prefixSum[i] + (isValid ? value : 0.0);
+            prefixCount[i + 1] = prefixCount[i] + (isValid ? 1 : 0);
         }
     }
 
     // [left, right)
+    public double querySum(int left, int right) {
+        return prefixSum[right] - prefixSum[left];
+    }
+
+    // [left, right)
+    public int queryCount(int left, int right) {
+        return prefixCount[right] - prefixCount[left];
+    }
+
+    // [left, right)
     public double queryAvg(int left, int right) {
-        double sum = prefixSum[right] - prefixSum[left];
-        return sum / (right - left);
+        double sum = querySum(left, right);
+        int count = queryCount(left, right);
+        return count > 0 ? sum / count : Double.NaN;
     }
 }

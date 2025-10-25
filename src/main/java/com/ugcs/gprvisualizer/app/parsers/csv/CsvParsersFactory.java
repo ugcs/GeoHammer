561cc6d96a4f0bc6c1805254ee4f4c6aec962c5f
Evgeny Pisarenko <episarenko@ugcs.com>
2025-10-25 13:14:25 +0200

    Remove NMEA parser (#471)

--- src/main/java/com/ugcs/gprvisualizer/app/parsers/csv/CsvParsersFactory.java
+++ src/main/java/com/ugcs/gprvisualizer/app/parsers/csv/CsvParsersFactory.java
@@ -7,14 +7,11 @@ public class CsvParsersFactory {
 
     private static final String MAGDRONE_TEMPLATE_CODE = "magdrone";
     
-    private static final String NMEA_TEMPLATE_CODE = "nmea";
-
     public CsvParser createCsvParser(Template template) {
         Check.notNull(template);
 
         return switch (template.getCode()) {
             case MAGDRONE_TEMPLATE_CODE -> new MagDroneCsvParser(template);
-            case NMEA_TEMPLATE_CODE -> new NmeaCsvParser(template);
             default -> new CsvParser(template);
         };
     }

3a8fa99f3c11525c20c9f81c5a5c9a19eba404bc
Vladislav Groshkov <vgroshkov@ugcs.com>
2025-11-05 12:23:11 +0100

    Adjust DecimalFormat in HorizontalRulerDrawer and SensorLineChartXAxis for consistent number formatting

--- src/main/java/com/ugcs/gprvisualizer/app/axis/SensorLineChartXAxis.java
+++ src/main/java/com/ugcs/gprvisualizer/app/axis/SensorLineChartXAxis.java
@@ -34,7 +34,7 @@ public class SensorLineChartXAxis extends ValueAxis<Number> {
 
     private final TemplateSettingsModel templateSettings;
 
-    private final DecimalFormat formatter = new DecimalFormat();
+    private final DecimalFormat formatter = new DecimalFormat("#0.00");
 
     private final CsvFile file;
 

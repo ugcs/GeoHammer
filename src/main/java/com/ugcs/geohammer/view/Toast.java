d375f887b784fd012a715c1cf21bb0772265fb91
Evgeny Pisarenko <episarenko@ugcs.com>
2025-12-10 13:33:44 +0100

    Hide previous toast when a new instance appears

--- src/main/java/com/ugcs/geohammer/view/Toast.java
+++ src/main/java/com/ugcs/geohammer/view/Toast.java
@@ -10,22 +10,48 @@ import javafx.util.Duration;
 public class Toast {
 
 	private static final int VERTICAL_OFFSET = 30;
-	private static final int DEFAULT_DISPLAY_TIME_MILLIS = 1000;
+
+    private static final int DEFAULT_DISPLAY_TIME_MILLIS = 1000;
+
+    private static final Label toastLabel = createToastLabel();
+
+    private static final Popup toast = createToast(toastLabel);
+
+    private static Timeline timeline;
+
+    private static Label createToastLabel() {
+        Label label = new Label();
+        label.setStyle(
+                "-fx-background-color: rgba(0,0,0,0.75); " +
+                "-fx-text-fill: white; " +
+                "-fx-padding: 8px; " +
+                "-fx-border-radius: 4px; " +
+                "-fx-background-radius: 4px;");
+        return label;
+    }
+
+    private static Popup createToast(Label toastLabel) {
+        Popup toast = new Popup();
+        toast.getContent().add(toastLabel);
+        toast.setAutoFix(true);
+        toast.setAutoHide(true);
+        return toast;
+    }
 
 	public static void show(String message, Node owner, double screenX, double screenY) {
 		show(message, owner, screenX, screenY, DEFAULT_DISPLAY_TIME_MILLIS);
 	}
 
 	public static void show(String message, Node owner, double screenX, double screenY, int millis) {
-		Label label = new Label(message);
-		label.setStyle("-fx-background-color: rgba(0,0,0,0.75); -fx-text-fill: white; -fx-padding: 8px; -fx-border-radius: 4px; -fx-background-radius: 4px;");
-		Popup toast = new Popup();
-		toast.getContent().add(label);
-		toast.setAutoFix(true);
-		toast.setAutoHide(true);
-		toast.show(owner, screenX, screenY - VERTICAL_OFFSET);
-
-		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(millis), ae -> toast.hide()));
-		timeline.play();
+        synchronized (toast) {
+            if (timeline != null) {
+                timeline.stop();
+            }
+            toast.hide();
+            toastLabel.setText(message);
+            toast.show(owner, screenX, screenY - VERTICAL_OFFSET);
+            timeline = new Timeline(new KeyFrame(Duration.millis(millis), ae -> toast.hide()));
+            timeline.play();
+        }
 	}
 }

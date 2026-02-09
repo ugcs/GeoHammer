80f1a3bf15e5ee1526287399fcf54bf033b65fb6
VGroshkov <vgroshkov@ugcs.com>
2026-02-09 17:38:26 +0100

    Add script parameter validation to ensure required fields are checked

---
+++ src/main/java/com/ugcs/geohammer/service/script/ScriptValidationException.java
@@ -0,0 +1,7 @@
+package com.ugcs.geohammer.service.script;
+
+public class ScriptValidationException extends Exception {
+	public ScriptValidationException(String message) {
+		super(message);
+	}
+}
